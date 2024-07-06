b52f4f4dbaea7f25e9e8c6abae72dae967daa82b
Boburmirzo <symonbobur@gmail.com>
2024-07-06 22:00:12 +0500

    Loading documents

--- utils/document.go
+++ utils/document.go
@@ -1,2 +1,40 @@
 package utils
 
+import (
+	"compress/gzip"
+	"encoding/xml"
+	"os"
+)
+
+type Document struct{
+    Title string `xml:"title"`
+    URL string     `xml:"url"`
+    Text   string `xml:"absctract"`
+    ID int 
+}
+
+func LoadDocument(path string) ([]Document, error)  {
+    f, err := os.Open(path)
+    if err != nil {
+        return nil, err
+    }
+    gz, err := gzip.NewReader(f)
+    if err != nil {
+        return nil, err 
+    }
+    defer gz.Close()
+    decoder := xml.NewDecoder(gz)
+    dump := struct{
+        Documents []Document `xml:"doc"`
+    }{}
+    if err := decoder.Decode(&dump); err != nil {
+        return nil, err
+    }
+    docs := dump.Documents
+    for i := range docs{
+        docs[i].ID = i
+    }
+    return docs, nil
+
+}
+
