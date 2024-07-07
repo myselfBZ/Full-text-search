1059f721a13b82e135780be5ec91b7793808c17a
Boburmirzo <symonbobur@gmail.com>
2024-07-07 22:16:31 +0500

    Analyzing string and cleaning it

--- utils/filter.go
+++ utils/filter.go
@@ -1,1 +1,31 @@
 package utils
+
+import "strings"
+
+
+
+func lowercaseFilter(text []string) []string {
+    r := make([]string, len(text))
+    for i, token := range text{
+        r[i] = strings.ToLower(token)
+    }
+    return r 
+}
+
+func stopwordFilter(tokens []string) []string  {
+    stopwords := map[string]struct{}{
+        "a":{},"and":{},"be":{},"have":{},"i":{},
+        "in":{},"of":{},"that":{},"the":{}, "to":{},
+    }
+    r := make([]string, len(tokens))
+    for _, token := range tokens{
+        if _, ok := stopwords[token]; !ok{
+            r = append(r, token)
+        }
+    }
+    return r 
+}
+
+func stemmerFilter(tokens []string) []string{
+
+}
