ccba6d72ca070f053b3741f48fa46729a6221613
Boburmirzo <symonbobur@gmail.com>
2024-07-07 22:18:05 +0500

    cleaning the data

--- utils/index.go
+++ utils/index.go
@@ -1,1 +1,11 @@
 package utils
+
+type Index map[string][]int
+
+func (i *Index) Add(docs []Document)  {
+    for _, d := range docs{
+        for _, token := range analyze(d.Text){
+            ids := i[token]
+        }
+    }
+}
